2e54a33cc3a0beeab84dd6e75aa735c7dc15e2e6
Unknown <ns1228yt@gmail.com>
2020-01-17 23:27:42 +0000

    Space Looter 0.6 - AI work (Nil)

--- Assets/Nil_Scripts/Nil_FOVdetection.cs
+++ Assets/Nil_Scripts/Nil_FOVdetection.cs
@@ -1,9 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Nil_FOVdetection : MonoBehaviour
 {
+   
+    public NavMeshAgent agent;
+
+
+   
+
+    public enum State
+    {
+        PATROL,
+        CHASE,
+        SEARCH,
+        INVESTIGATE
+
+
+    }
+
+
+
+    public State state;
+    private bool alive;
+
+
+    public float speed = 10f;
+   
+
+    public List<Transform> wayPointsList = new List<Transform>();
+
+    public Transform currentWaypoint;
+
+    public int wayPointNumber = 0;
+
+    
+
+
 
     public Transform Player;
     public float maxAngle;
@@ -27,29 +63,29 @@ public class Nil_FOVdetection : MonoBehaviour
             Gizmos.color = Color.red;
         else
             Gizmos.color = Color.green;
-        Gizmos.DrawRay(transform.position, (Player.position-transform.position).normalized *maxRadius);
+        Gizmos.DrawRay(transform.position, (Player.position - transform.position).normalized * maxRadius);
 
         Gizmos.color = Color.black;
-        Gizmos.DrawRay(transform.position, transform.forward *maxRadius);
+        Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
     }
 
-    public static bool inFOV (Transform checkingObject, Transform target, float maxAngle, float maxRadius)
+    public static bool inFOV(Transform checkingObject, Transform target, float maxAngle, float maxRadius)
     {
         Collider[] overlaps = new Collider[10];
         int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overlaps);
 
         for (int i = 0; i < count + 1; i++)
         {
-            if(overlaps[i] != null)
+            if (overlaps[i] != null)
             {
-                if(overlaps[i].transform == target)
+                if (overlaps[i].transform == target)
                 {
                     Vector3 directionBetween = (target.position - checkingObject.position).normalized;
                     directionBetween.y *= 0;
 
                     float angle = Vector3.Angle(checkingObject.forward, directionBetween);
 
-                    if(angle <=  maxAngle)
+                    if (angle <= maxAngle)
                     {
                         Ray ray = new Ray(checkingObject.position, target.position - checkingObject.position);
                         RaycastHit hit;
@@ -57,33 +93,99 @@ public class Nil_FOVdetection : MonoBehaviour
                         if (Physics.Raycast(ray, out hit, maxRadius))
                         {
                             if (hit.transform == target)
-                            return true;
+                                return true;
                         }
                     }
-                        
+
                 }
             }
-        }     
-             
+        }
+
 
         return false;
     }
-        
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        agent = GetComponent<NavMeshAgent>();
+        state = Nil_FOVdetection.State.PATROL;
+        alive = true;
+
+        StartCoroutine("FSM");
+
+
+        currentWaypoint = wayPointsList[wayPointNumber];
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+    
+
         //checks if in fov (bool)
         isinFov = inFOV(transform, Player, maxAngle, maxAngle);
 
-        if(isinFov)
+        if (isinFov)
         {
-            
+
         }
+        //Vector3 relativePos = currentWaypoint.position - transform.position;
+       // transform.rotation = Quaternion.LookRotation(relativePos);
+
+
     }
+
+    IEnumerator FSM()
+    {
+        while (alive)
+        {
+            switch (state)
+            {
+                case State.PATROL:
+                    Patrol();
+                    break;
+            }
+            yield return null;
+
+        }
+
+
+    }
+
+    void Patrol()
+    {
+        float distanceToCurrent = Vector2.Distance(transform.position, currentWaypoint.position);
+        
+       
+            if (distanceToCurrent == 0)
+        {
+            if (wayPointNumber != wayPointsList.Count - 1)
+            {
+                wayPointNumber++;
+                currentWaypoint = wayPointsList[wayPointNumber];
+            }
+            else
+            {
+                wayPointNumber = 0;
+                currentWaypoint = wayPointsList[wayPointNumber];
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, speed * Time.deltaTime);
+
+
+
+    } 
 }
+
+
+    
+
+
