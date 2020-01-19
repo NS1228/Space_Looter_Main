4882e9f3dc912ada46ef2435e608b014b367f5f6
Lemar <lb1055@live.mdx.ac.uk>
2020-01-19 17:14:47 +0000

    Space Looter 0.8 - Player Movement Plus Camera
    Left Shift to control only camera
    Space to sprint

---
+++ Assets/Lemar Assets/LemarCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LemarCamera : MonoBehaviour
+{
+    public float RotateSpeed = 1;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * RotateSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * RotateSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+          Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+
+    }
+}
---
+++ Assets/Lemar Assets/LemarMovment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LemarMovment : MonoBehaviour
+{
+    public float Speed;
+    public float Sprint;
+    public float Walk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprint = Speed * 2;
+        Walk = Speed * 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(); 
+    }
+    void Movement()
+    {
+        float horizon = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horizon, 0f ,verti) * Speed * Time.deltaTime;
+        transform.Translate(move, Space.Self);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Speed = Sprint;
+        }
+        else
+        {
+            Speed = Walk;
+        }
+    }
+}
