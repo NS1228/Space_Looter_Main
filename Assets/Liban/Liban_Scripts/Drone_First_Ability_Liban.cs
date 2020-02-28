0eba4ffc1922f180e387586966da80d39f5a9fcb
Liban <LA627@live.mdx.ac.uk>
2020-02-28 00:27:11 +0000

    Space Looter 8.6 - Started on Drone Input - Liban

---
+++ Assets/Liban/Liban_Scripts/Drone_First_Ability_Liban.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Drone_First_Ability_Liban : MonoBehaviour
+{
+
+
+    public Transform ThaaaEnemyy;
+
+    public Text EnemyFrozenText;
+
+
+
+
+
+    void Update()
+    {
+
+
+
+
+        if(Input.GetMouseButtonDown(0))
+
+
+        {
+
+
+            StartCoroutine(StartTheFirstAbility());
+
+
+
+        }
+
+
+
+        
+    }
+
+
+
+
+    IEnumerator StartTheFirstAbility()
+
+
+    {
+
+
+        ThaaaEnemyy.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+
+
+
+
+        ActivateWatchAbility ERT = GameObject.Find("Activate the watch ability").GetComponent<ActivateWatchAbility>();
+
+        ERT.enabled = false;
+
+
+
+
+
+
+
+
+        Light_Hold_Blue JKL = GameObject.FindWithTag("FlashLight").GetComponent<Light_Hold_Blue>();
+
+
+        JKL.enabled = false;
+
+
+
+
+
+
+        Drone_First_Ability_Liban F2367 = GameObject.Find("Activate Drone first ability").GetComponent<Drone_First_Ability_Liban>();
+
+
+        F2367.enabled = false;
+
+
+
+
+        EnemyFrozenText.gameObject.SetActive(true);
+
+
+
+
+
+
+        yield return new WaitForSeconds(5.0f);
+
+
+
+
+
+
+
+        ThaaaEnemyy.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+
+
+
+
+        ActivateWatchAbility TYU = GameObject.Find("Activate the watch ability").GetComponent<ActivateWatchAbility>();
+
+
+        TYU.enabled = true;
+
+
+
+
+
+
+
+
+        EnemyFrozenText.gameObject.SetActive(false);
+
+
+
+
+
+
+
+
+
+        yield return new WaitForSeconds(5.5f);
+
+
+
+
+
+
+
+
+        Drone_First_Ability_Liban PDFG = GameObject.Find("Activate Drone first ability").GetComponent<Drone_First_Ability_Liban>();
+
+        PDFG.enabled = true;
+
+
+
+
+
+
+
+
+        Light_Hold_Blue QAS = GameObject.FindWithTag("FlashLight").GetComponent<Light_Hold_Blue>();
+
+
+        QAS.enabled = true;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Assets/Liban/Liban_Scripts/Drone_Second_ability_Liban.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone_Second_ability_Liban : MonoBehaviour
+{
+    
+
+
+
+    void Update()
+    {
+        
+
+        if(Input.GetMouseButtonDown(1))
+
+        {
+
+
+            StartCoroutine(ActivateExposeLocations());
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+    IEnumerator ActivateExposeLocations()
+
+
+
+    {
+
+
+
+        Outline O1 = GameObject.Find("enemy sphere 2").GetComponent<Outline>();
+
+        O1.enabled = true;
+
+
+
+
+
+
+        Outline O2 = GameObject.Find("enemy sphere 3").GetComponent<Outline>();
+
+
+        O2.enabled = true;
+
+
+
+
+
+        Outline O3 = GameObject.Find("enemy sphere 4").GetComponent<Outline>();
+
+
+        O3.enabled = true;
+
+
+
+
+
+        Drone_Second_ability_Liban XCV = GameObject.Find("Activate Drone second ability").GetComponent<Drone_Second_ability_Liban>();
+
+
+        XCV.enabled = false;
+
+
+
+
+
+        Stun_Light_Liban Stunnnn = GameObject.FindWithTag("FlashLight").GetComponent<Stun_Light_Liban>();
+
+
+        Stunnnn.enabled = false;
+
+
+
+
+
+
+        ActivateWatchAbilityTwo_Liban AWQ123 = GameObject.Find("Activate second watch ability").GetComponent<ActivateWatchAbilityTwo_Liban>();
+
+
+        AWQ123.enabled = false;
+
+
+
+
+
+
+
+
+
+        yield return new WaitForSeconds(5.0f);
+
+
+
+
+
+
+
+
+
+
+
+        Outline O12 = GameObject.Find("enemy sphere 2").GetComponent<Outline>();
+
+
+        O12.enabled = false;
+
+
+
+
+
+
+        Outline O13 = GameObject.Find("enemy sphere 3").GetComponent<Outline>();
+
+
+        O13.enabled = false;
+
+
+
+
+
+
+        Outline O14 = GameObject.Find("enemy sphere 4").GetComponent<Outline>();
+
+
+        O14.enabled = false;
+
+
+
+
+
+        Stun_Light_Liban Stttttun = GameObject.FindWithTag("FlashLight").GetComponent<Stun_Light_Liban>();
+
+
+        Stttttun.enabled = true;
+
+
+
+
+
+
+
+
+
+
+
+        yield return new WaitForSeconds(5.7f);
+
+
+
+
+
+
+
+        Drone_Second_ability_Liban ZXC = GameObject.Find("Activate Drone second ability").GetComponent<Drone_Second_ability_Liban>();
+
+
+        ZXC.enabled = true;
+
+
+
+
+
+
+
+
+        ActivateWatchAbilityTwo_Liban TGB = GameObject.Find("Activate second watch ability").GetComponent<ActivateWatchAbilityTwo_Liban>();
+
+        TGB.enabled = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
