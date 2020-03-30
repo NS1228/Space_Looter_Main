3b024f1bf3130cf9c9afbd55358868793abb4906
Liban <LA627@live.mdx.ac.uk>
2020-03-30 22:05:27 +0100

    Space Looter 14.4 - More Adjustments - Liban

---
+++ Assets/Liban/Liban_Scripts/audio_random_tester.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class audio_random_tester : MonoBehaviour
+{
+
+    public AudioClip [] B3;
+
+    public AudioSource B2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(Input.GetKeyDown(KeyCode.P))
+
+        {
+
+
+            B2.clip = B3[Random.Range(0, B3.Length)];
+
+
+            B2.Play();
+
+
+        }
+
+        
+    }
+}
