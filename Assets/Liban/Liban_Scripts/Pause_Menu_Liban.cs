61790b1a7c3aa10df677fbacd27234b949cbe4ab
Liban <LA627@live.mdx.ac.uk>
2020-05-09 22:24:00 +0100

    Space Looter 19.5 - Pause Menu Implemented - Liban

---
+++ Assets/Liban/Liban_Scripts/Pause_Menu_Liban.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu_Liban : MonoBehaviour
+{
+
+    public bool isThePauseMenuOn;
+
+    public GameObject PauseMenuu;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+
+
+        {
+
+
+            isThePauseMenuOn = !isThePauseMenuOn;
+
+
+        }
+
+
+
+
+
+
+
+        if(isThePauseMenuOn)
+
+
+        {
+
+
+
+
+            PauseMenuu.SetActive(true);
+
+
+            Time.timeScale = 0;
+
+
+            LemarCamera HJUYP = GameObject.FindWithTag("MainCamera").GetComponent<LemarCamera>();
+
+            HJUYP.RotateSpeed = 0;
+
+
+        }
+
+
+        else
+
+
+
+
+
+        {
+
+
+
+            PauseMenuu.SetActive(false);
+
+            Time.timeScale = 1;
+
+
+            LemarCamera HJUPYT = GameObject.FindWithTag("MainCamera").GetComponent<LemarCamera>();
+
+            HJUPYT.RotateSpeed = 10;
+
+        }
+
+
+
+    }
+}
